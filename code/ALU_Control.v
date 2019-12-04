a5cd177bb8c890964a2d0c646c0924c0e15cdd6a
b06902077 <b06902077@linux13.csie.ntu.edu.tw>
2019-12-04 21:32:40 +0800

    Control

--- code/ALU_Control.v
+++ code/ALU_Control.v
@@ -10,7 +10,9 @@ input [1:0] ALUOp_i;
 output [2:0] ALUCtrl_o;
 
 assign ALUCtrl_o[2] = funct_i[2];
-assign ALUCtrl_o[1] = (funct_i[8] == 1) ? 1 : funct_i[1];
-assign ALUCtrl_o[0] = (funct_i[3] == 1) ? 1 : funct_i[0];
+assign ALUCtrl_o[1] = (!ALUOp_i[0]) ? 0 :
+					  (funct_i[8]) ? 1 : funct_i[1];
+assign ALUCtrl_o[0] = (!ALUOp_i[0]) ? 0 :
+					  (funct_i[3]) ? 1 : funct_i[0];
 
 endmodule
--- code/Control.v
+++ code/Control.v
@@ -1,18 +1,23 @@
 module Control
 (
 	Op_i,
-	ALUOp_o,
-	ALUSrc_o,
-	RegWrite_o
+	Zero_i,
+	Signal_o,
+	flush_o
 );
 
-input [6:0] Op_i;
-output [1:0] ALUOp_o;
-output ALUSrc_o;
-output RegWrite_o;
+input  [6:0] Op_i;
+input        Zero_i;
+output [6:0] Signal_o;
+output       flush_o;
 
-assign ALUOp_o = 2'b01;
-assign ALUSrc_o = ~Op_i[5];
-assign RegWrite_o = 1;
+assign flush_o = (Zero_i && Op_i[6]) ? 1 : 0;
+assign Signal_o[6] = (Op_i[4]) ? 1 : Op_i[5];
+assign Signal_o[5] = Op_i[4];
+assign Signal_o[4] = (!Op_i[4] || !Op_i[5]) ? 1 : 0;
+assign Signal_o[3] = (Op_i[5:4] == 2'b00) ? 1 : 0;
+assign Signal_o[2] = (Op_i[6:4] == 3'b010) ? 1 : 0;
+assign Signal_o[1] = (Op_i[5:4] != 2'b10) ? 1 : 0;
+assign Signal_o[0] = (Op_i[5:4] == 2'b00) ? 1 : 0;
 
 endmodule

bb393dd197698461c1d5fe51e7c5f7f68b3cd8be
b06902077 <b06902077@linux13.csie.ntu.edu.tw>
2019-12-08 17:30:01 +0800

    a

--- code/Sign_Extend.v
+++ code/Sign_Extend.v
@@ -7,8 +7,8 @@ module Sign_Extend
 input [31:0] data_i;
 output [31:0] data_o;
 
-assign data_o[12:0] = (!data_i[5]) ? {data_i[31],data_i[31:20]} :
-					  (!data_i[6]) ? {data_i[31],data_i[31:25],data_i[11:7]} : {data_i[31],data_i[7],data_i[30,25],data_i[11:8],0};
-assign data_o[31:13] = {19{data_o[12]}};
+assign data_o[11:0] = (!data_i[5]) ? data_i[31:20] :
+					  (!data_i[6]) ? {data_i[31:25],data_i[11:7]} : {data_i[31],data_i[7],data_i[30,25],data_i[11:8]};
+assign data_o[31:12] = {20{data_o[12]}};
 
 endmodule
